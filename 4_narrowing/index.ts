91561378d4eac1f574568bdb28d6d034a2885c41
Flávio Félix <fau_felix30@hotmail.com>
2024-06-18 20:15:33 -0300

    Começando o narrowing

---
+++ 4_narrowing/index.ts
@@ -0,0 +1,24 @@
+// 1 - Type guard
+function sum(a: number | string, b: number | string): number | string | null {
+  const numA = Number(a);
+  const numB = Number(b);
+
+  if (
+    isNaN(numA) ||
+    isNaN(numB) ||
+    (typeof a === "string" && isNaN(parseFloat(a))) ||
+    (typeof b === "string" && isNaN(parseFloat(b)))
+  ) {
+    return null;
+  }
+
+  const result = numA + numB;
+  return typeof a === "string" || typeof b === "string"
+    ? result.toString()
+    : result;
+}
+
+console.log(sum(1, 2));
+console.log(sum("1", 2));
+console.log(sum("1", "2"));
+console.log(sum("a", "2"));
